3d410a1d8a860f5fa7a6f784193fca3d303c3f61
Salzie <32619234+Salzie@users.noreply.github.com>
2025-08-04 15:10:31 +0530

    Indentation Model

---
+++ run.py
@@ -0,0 +1,33 @@
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
+
+# dimensions of our images
+img_width, img_height = 128, 128
+
+# load the model we saved
+model = load_model('first_model.h5')
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# predicting images
+img = image.load_img('test4.png', target_size=(img_width, img_height))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+images = np.vstack([x])
+classes = model.predict_classes(images, batch_size=10)
+print classes
+
+# predicting multiple images at once
+img = image.load_img('test3.png', target_size=(img_width, img_height))
+y = image.img_to_array(img)
+y = np.expand_dims(y, axis=0)
+
+# pass the list of multiple images np.vstack()
+images = np.vstack([x, y])
+classes = model.predict_classes(images, batch_size=10)
+
+# print the classes, the images belong to
+print classes
